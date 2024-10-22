b7d4fff6c84bf1b1ae7ea329949f2ab796182585
Baptiste Caramiaux <baptiste.caramiaux@gmail.com>
2024-10-22 18:27:09 +0200

    model selection

---
+++ infer_loras.py
@@ -0,0 +1,18 @@
+from diffusers import DiffusionPipeline
+import torch
+
+pipe_id = "stabilityai/stable-diffusion-xl-base-1.0"
+pipe = DiffusionPipeline.from_pretrained(pipe_id, torch_dtype=torch.float16).to("cuda")
+
+pipe.load_lora_weights(weight_name="./Loras/lauras/output/lauras-000002.safetensors") 
+
+prompt = "ceramic divorce"
+
+lora_scale = 1.2
+image = pipe(
+    prompt, 
+    num_inference_steps=30, 
+    cross_attention_kwargs={"scale": lora_scale}, 
+    generator=torch.manual_seed(0)
+).images[0]
+
