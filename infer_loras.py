1eaf4ffe30f8c688c14f9aaeb81444f6df3f706d
Baptiste Caramiaux <baptiste.caramiaux@gmail.com>
2024-10-22 19:01:55 +0200

    model selection

--- infer_loras.py
+++ infer_loras.py
@@ -6,7 +6,9 @@ import torch
 # pipe = StableDiffusionXLPipeline.from_single_file("./sd_xl_base_1.0.safetensors").to("cuda")
 pipe = StableDiffusionPipeline.from_single_file("./v1-5-pruned-emaonly.safetensors").to("cuda")
 
-pipe.load_lora_weights(weight_name="./Loras/lauras/output/lauras-000002.safetensors") 
+pipe.load_lora_weights(
+    "./Loras/lauras/output/lauras-000002.safetensors",
+    weight_name="lauras-000002.safetensors") 
 
 prompt = "ceramic divorce"
 
